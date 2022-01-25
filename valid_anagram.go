25d4d98990ec90e1bd036fbb07b97999ecfd7d8b
rsh-codes <95175297+rsh-codes@users.noreply.github.com>
2022-01-25 10:22:00 -0600

    Create valid_anagram.go

---
+++ valid_anagram.go
@@ -0,0 +1,16 @@
+func isAnagram(s string, t string) bool {
+    if len(s) != len(t) {
+		return false
+	}
+	m := [26]int{}
+	for i := range s {
+		m[int(s[i]-'a')]++
+		m[int(t[i]-'a')]--
+	}
+	for i := range m {
+		if m[i] != 0 {
+			return false
+		}
+	}
+	return true
+}
