9d26f5a026702227bacbb629146332a3b2bc0fe5
rsh-codes <95175297+rsh-codes@users.noreply.github.com>
2022-02-01 14:07:25 -0600

    Create climb_stairs.go

---
+++ go solutions/climb_stairs.go
@@ -0,0 +1,13 @@
+func climbStairs(n int) int {
+    if n == 0 {
+		return 0
+	}
+	if n == 1 || n == 2 {
+		return n
+	}
+	step := []int{1, 2}
+	for i := 2; i < n; i++ {
+		step = append(step, step[i-1]+step[i-2])
+	}
+	return step[n-1]
+}
