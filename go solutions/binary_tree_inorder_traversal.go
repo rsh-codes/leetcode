0003feebc041b0a9c7bc898e47b43b30f4a6db2a
rsh-codes <95175297+rsh-codes@users.noreply.github.com>
2022-02-07 08:54:14 -0600

    Create binary_tree_inorder_traversal.go

---
+++ go solutions/binary_tree_inorder_traversal.go
@@ -0,0 +1,18 @@
+func inorderTraversal(root *TreeNode) []int {
+	p := root
+	ret := []int{}
+	stack := []*TreeNode{}
+	for p != nil || len(stack) != 0 {
+		for p != nil {
+			stack = append(stack, p)
+			p = p.Left
+		}
+		if len(stack) != 0 {
+			tmp := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			p = tmp.Right
+			ret = append(ret, tmp.Val)
+		}
+	}
+	return ret
+}
