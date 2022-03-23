c1703115ea81c52fdfe43e052a457325c6c25565
rsh-codes <95175297+rsh-codes@users.noreply.github.com>
2022-03-23 09:42:49 -0500

    Create InsertIntoBinarySearchTree.cs

---
+++ C sharp solutions/InsertIntoBinarySearchTree.cs
@@ -0,0 +1,50 @@
+namespace LeetCode
+{
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     public int val;
+     *     public TreeNode left;
+     *     public TreeNode right;
+     *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+     *         this.val = val;
+     *         this.left = left;
+     *         this.right = right;
+     *     }
+     * }
+     */
+    public class _0701_InsertIntoABinarySearchTree
+    {
+        public TreeNode InsertIntoBST(TreeNode root, int val)
+        {
+            if (root == null) return new TreeNode(val);
+
+            var node = root;
+            while (node != null)
+            {
+                if (val < node.val)
+                {
+                    if (node.left == null)
+                    {
+                        node.left = new TreeNode(val);
+                        return root;
+                    }
+                    else
+                        node = node.left;
+                }
+                else
+                {
+                    if (node.right == null)
+                    {
+                        node.right = new TreeNode(val);
+                        return root;
+                    }
+                    else
+                        node = node.right;
+                }
+            }
+
+            return root;
+        }
+    }
+}
